c0453f70fa2b8a2529914faf81a755d57c20cb70
Luis Weitl <weitlluis@proton.me>
2025-03-18 21:49:20 +0100

    Added placeholder stuff. Added final 'game-code' to bin/src/main.rs. Just outputs hello world (again, placeholder). Filled lib.rs in crates/fexun/src/lib.rs (again, agin, placeholder)

---
+++ bin/src/main.rs
@@ -0,0 +1,9 @@
+//
+// While this doesn't do anything yes, this is how the main.rs for a FeXun game will eventually look like
+// Pretty neat, huh?
+//
+use fexun::prelude::*;
+
+fn main() {
+    fexun::run(game);
+}
--- crates/fexun/src/lib.rs
+++ crates/fexun/src/lib.rs
@@ -1,1 +1,12 @@
-
+pub mod prelude {                   //
+    pub use crate::run;             //
+    pub use crate::game;            //
+}                                   // Non functional code here
+                                    //
+pub fn run(game: fn()) {            // These are just placeholder functions and modules
+    game();                         // so Clippy doesn't annoy the shit
+}                                   // out of me...
+                                    //
+pub fn game() {                     //
+    println!("Hello, world!");      //
+}                                   //
