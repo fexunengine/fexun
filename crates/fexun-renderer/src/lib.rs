2663cd5eaeb5e1707265329cc742c8637f59cfe2
Luis Weitl <weitlluis@proton.me>
2025-06-09 15:33:42 +0200

    Update wgpu to V25 and made migrations

--- crates/fexun-renderer/src/lib.rs
+++ crates/fexun-renderer/src/lib.rs
@@ -18,7 +18,7 @@ impl WgpuState<'_> {
             .await
             .unwrap();
         let (device, queue) = adapter
-            .request_device(&wgpu::DeviceDescriptor::default(), None)
+            .request_device(&wgpu::DeviceDescriptor::default())
             .await
             .unwrap();
 
