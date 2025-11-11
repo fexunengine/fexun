babebcc80c70b11bec5c3794db22533e91a8d1a3
Luis Weitl <weitlluis@proton.me>
2025-11-11 21:30:53 +0100

    Add incredibly funny comments

--- fexun-runtime/src/lib.rs
+++ fexun-runtime/src/lib.rs
@@ -1,5 +1,7 @@
 use fexun_core;
 
+// Ermm... `unsafe` is short for "perfect for prod" chat 🤓☝
+// Hmmmm... "FeXun [...] emphasizes safety", meanwhile:
 #[unsafe(no_mangle)]
 pub extern "C" fn fexun_run() {
     fexun_core::main();
